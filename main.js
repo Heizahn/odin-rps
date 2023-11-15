108abff2ae31d80bf2b02d38aa20a52c9cf1cecf
Heizahn <humbertorbw@gmail.com>
2023-11-15 05:31:34 -0400

    game complete

--- main.js
+++ main.js
@@ -32,5 +32,33 @@ function determineWinner(userSelection, computerSelection){
 }
 
 
-let userSelection = prompt('Choose rock, paper, or scissors: ');
-console.log(determineWinner(userSelection, getComputerChoice()));
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    for(let i = 0; i < 5; i++){
+        let result = determineWinner(prompt('Choose rock, paper, or scissors: '), getComputerChoice());
+        if(result === 'The game was a tie!'){
+            scoreComputer = scoreComputer
+            scorePlayer = scorePlayer
+            alert('The game was a tie!')
+        } else if (result === 'You win!'){
+            scorePlayer++
+            alert('You win!')
+        } else {
+            scoreComputer++
+            alert('You lose!')
+        }
+    }
+
+    if(scorePlayer > scoreComputer){
+        alert('You win the game!');
+    } else if(scorePlayer < scoreComputer){
+        alert('You lose the game!');
+    } else {
+        alert('The game was a tie!');
+    }
+}
+
+game();
