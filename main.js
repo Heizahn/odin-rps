0b470bc830f431d969309649adf5611c3340f2b3
Heizahn <humbertorbw@gmail.com>
2023-11-15 05:17:24 -0400

    function determineWinner create and complete

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ function getComputerChoice(){
     return choices[randomNumber];
 }
 
+function determineWinner(userSelection, computerSelection){
+    userSelection = userSelection.toLowerCase();
+
+    if(userSelection === computerSelection){
+        return 'The game was a tie!';
+    } else if(userSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            return 'You lose!, Paper beats Rock';
+        } else {
+            return 'You win!';
+        }
+    } else if(userSelection === 'paper'){
+        if(computerSelection === 'scissors'){
+            return 'You lose!, Scissors beats Paper';
+        } else {
+            return 'You win!';
+        }
+    } else if(userSelection === 'scissors'){
+        if(computerSelection === 'rock'){
+            return 'You lose!, Rock beats Scissors';
+        } else {
+            return 'You win!';
+        }
+    }
+}
+
+
+let userSelection = prompt('Choose rock, paper, or scissors: ');
+console.log(determineWinner(userSelection, getComputerChoice()));
